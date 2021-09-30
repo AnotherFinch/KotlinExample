9d0e7a7e5ed18f27f7864b7ac64f414c36e4e01b
Virtual Tester <mikhail.stepanc.henko@open.ru>
2021-09-30 13:51:43 +0300

    Начало

---
+++ app/src/main/java/com/example/my_application_on_kontlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.my_application_on_kontlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
